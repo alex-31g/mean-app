4143e29e30eb016f1bd0a1f4f39f21bc9d186b5b
alex-31g <al.tsimbalyuk@gmail.com>
2020-11-27 23:50:12 +0200

    add

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const app = require('./app')
 const port = process.env.PORT || 5000
+const mongoose = require('mongoose')
 
-app.listen(port, () => console.log(`server work at ${port} port`))
+async function start() {
+	try {
+		await mongoose.connect('mongodb+srv://alex:%2a111%23@cluster0.a2m34.mongodb.net/meandb', {
+			useNewUrlParser: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true 
+		})
+		app.listen(port, () => console.log(`server work at ${port} port`))
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+start()
